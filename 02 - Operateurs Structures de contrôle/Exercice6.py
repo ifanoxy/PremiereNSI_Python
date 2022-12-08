744785b4cd37433e4a46d09554c99058c0b44124
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-08 21:49:28 +0100

    Create Exercice6.py

---
+++ 02 - Operateurs Structures de contrôle/Exercice6.py
@@ -0,0 +1,3 @@
+largeur, hauteur, profondeur = input('Entrez la valeur de la largeur'), input('Entrez la valeur de la hauteur'), input('Entrez la valeur de la profondeur')
+
+print(f'le volume du parallélépipède rectangle = {largeur*hauteur*profondeur}')
