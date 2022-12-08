fe1ac1a1a726aaaf0fce4fe9c9f21c448ada6b14
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-08 21:34:34 +0100

    Create Exercice1.py

---
+++ 01 - Structure d’un programme-notion de variable/Exercice1.py
@@ -0,0 +1,6 @@
+# Décrivez-le plus clairement et le plus complètement possible ce qui se passe à chacune des trois lignes de l'exemple ci-dessous;
+# de quel type sont les 3 variables ? :
+
+largeur = 20 # Type = Integer
+hauteur = 5 * 9.3 # Type = Float
+largeur * hauteur # Type = Float
