30d127f6fc94900f0c88cf45c9dfe8973f1300a7
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-09 10:23:22 +0100

    Create Exercice1.py

---
+++ 03 - Types de donnees de base/Exercice1.py
@@ -0,0 +1,44 @@
+# Affectez de façon parallèle deux entiers a et b de façon à ce que a=5 et b = 3.8 puis affichez la valeur de la somme.
+# Ajoutez 1 à a et affichez sa valeur.
+# Otez 1 à a et affichez sa valeur.
+# Multiplier a par 2 et affichez sa valeur.
+# Diviser a par 2 et affichez sa valeur.
+# Elever a à l’exposant 3 et affichez sa valeur.
+# Calculer la racine carré de a et affichez sa valeur.
+# Calculer le quotient de la division euclidienne de a par b et affichez sa valeur.
+# Calculer le reste de la division euclidienne de a par b et affichez sa valeur.
+
+a, b = 5, 3.8
+print(a+b)
+
+a += 1
+
+print(a)
+
+a -= 1
+
+print(a)
+
+a *= 2
+
+print(a)
+
+a /= 2
+
+print(a)
+
+a **= 3 
+
+print(a)
+
+a **= 0.5
+
+print(a)
+
+a //= b
+
+print(a)
+
+a %= b
+
+print(a)
