62813f4e905bf02cce5b484c7cb0b2666b41f24b
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-09 12:33:44 +0100

    Create Exercice7.py

---
+++ 04 - Types de donnees construits listes/Exercice7.py
@@ -0,0 +1,6 @@
+# Écrivez un programme qui affiche « proprement » tous les éléments d’une liste. 
+# Si on l’appliquait par exemple à la liste t2 de l’exercice précédent, 
+# on devrait obtenir : Janvier Février Mars Avril Mai Juin Juillet Août Septembre Octobre Novembre Décembre 
+
+t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+print(" ".join(t2))
