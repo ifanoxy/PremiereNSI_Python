8363b16f13584c82af6495b5236ab0290e3145c6
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-09 12:20:55 +0100

    Create Exercice6.py

---
+++ 04 - Types de donnees construits listes/Exercice6.py
@@ -0,0 +1,23 @@
+# Soient les listes suivantes : 
+# t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+# t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+
+# Écrivez un programme qui crée une nouvelle liste t3. 
+# Celle-ci devra contenir tous les éléments des deux listes en les alternant,
+# de telle manière que chaque nom de mois soit suivi du nombre de jours correspondant : 
+# ['Janvier',31,'Février',28,'Mars',31, ...].
+
+t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+t3 = list()
+i = 0
+
+maxlen = [len(t1), len(t2)]
+maxlen.reverse()
+
+while i < maxlen[0]:
+    t3.append(t2[i])
+    t3.append(t1[i])
+    i+=1
+
+print(t3)
