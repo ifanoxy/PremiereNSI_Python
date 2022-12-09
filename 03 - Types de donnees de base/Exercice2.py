61fd387f2351c0ae4b4843716ff9b0a61d478416
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-09 10:32:10 +0100

    Create Exercice2.py

---
+++ 03 - Types de donnees de base/Exercice2.py
@@ -0,0 +1,7 @@
+# Affectez de façon parallèle deux chaînes de caractères prenom et nom 
+# de façon à ce que prenom corresponde à Donovan et nom à Soubeyrant 
+# puis affichez la valeur de la somme appelée Concaténation.
+
+prenom, nom = "Donovan", "Soubeyrant"
+
+print(prenom + nom)
