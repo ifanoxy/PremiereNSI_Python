1eb5320ae0ccc86adeaa6710f4d363659ce19b49
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-09 10:33:41 +0100

    Create Exercice3.py

---
+++ 03 - Types de donnees de base/Exercice3.py
@@ -0,0 +1,7 @@
+# Déclarez la chaîne de caractères surnom à laquelle vous affecterez la valeur Dono
+# puis affichez la phrase : « Donovan Soubeyrant, surnommé Dono, habite à Epernay ».
+
+prenom, nom = "Donovan", "Soubeyrant"
+surnom = "Dono"
+
+print(f'{prenom} {nom}, surnommé ${surnom}, habite à Epernay')
