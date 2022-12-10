f02b21c391f82027491713b4415f5573f929a0fb
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-10 09:44:18 +0100

    Create Activite4.py

---
+++ 05 - les fonctions et procédures/Activite4.py
@@ -0,0 +1,10 @@
+# Ecrire une fonction moyenne qui reçoit trois nombres en paramètres
+# et qui retourne la moyenne avec un return puis tester son fonctionnement.
+
+def moyenne(nbr1, nbr2, nbr3):
+    """
+    Renvoie la moyenne de 3 nombres
+    """
+    return (nbr1+nbr2+nbr3)/3
+
+print(moyenne(7,10,13))
