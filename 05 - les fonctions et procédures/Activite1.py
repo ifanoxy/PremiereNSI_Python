8ac5f4a95ac761ff7d65670dab4c78cd15e3bb19
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-10 09:32:40 +0100

    Create Activite1.py

---
+++ 05 - les fonctions et procédures/Activite1.py
@@ -0,0 +1,9 @@
+def carree(a) :
+	""" 
+Fonction permettant de renvoyer le carré du nombre a qui est en paramètre
+	"""
+	return a**2 # renvoie l'image de a par la fonction carree
+
+# Faire un copier-coller du code la fonction carree() puis tester son bon fonctionnement en ajoutant en dessous de la fonction : print(carree(3))
+
+print(carree(3))
