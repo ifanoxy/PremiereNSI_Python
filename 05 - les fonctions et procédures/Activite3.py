6ce6b992ea0f0e0cfb3737201a5723794b91abca
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-10 09:42:31 +0100

    Create Activite3.py

---
+++ 05 - les fonctions et procédures/Activite3.py
@@ -0,0 +1,11 @@
+# Chercher l'erreur (il y a 3 erreurs à corriger). Mettre le corrigé en-dessous :
+
+# def quotient(x,y) <-- Manque les deux points
+# return x/y  <-- Manque l'indentation
+
+# print(carré(1.7)) <-- Appelle d'une fonction inexistante avec de mauvais arguements
+
+def quotient(x,y):
+    return x/y 
+
+print(quotient(1,7))
