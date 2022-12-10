d8b4ac7209bffde834b6a92672641f628fdfc265
ifanoxy <61195978+ifanoxy@users.noreply.github.com>
2022-12-10 09:39:37 +0100

    Create Activite2.py

---
+++ 05 - les fonctions et procédures/Activite2.py
@@ -0,0 +1,22 @@
+#  Tester le programme suivant :
+def affichage(mot1,mot2):
+    """
+    Affiche deux mots dans la console
+    """
+    print(mot1,mot2)
+
+
+affichage("NSI","Python")
+
+# Donnez les paramètres et les arguments de la fonction affichage :
+
+# paramètres :  mot1, mot2
+# arguements : "NSI", "Python"
+
+
+# Ajouter un docstring qui permet de décrire la procédure affichage()
+# puis tester son fonctionnement en exécutant le programme puis en tapant
+# help(affichage) dans la console.
+# Afficher ci-dessous ce que nous affiche la console dans ce cas :
+
+help(affichage)
